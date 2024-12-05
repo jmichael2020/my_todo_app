6db078045d137137893a104803a7954f557ff564
jjfm01 <jfino@jccm.es>
2024-12-05 20:33:28 +0100

    una ammed

--- web.py
+++ web.py
@@ -11,19 +11,25 @@ todos = get_todos()
 
 #session_state es como un dictionary, con parese clave valor
 
-
+#st.set_page_config(layout="wide") #para que responda a los cambios de tamaño
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     write_todos(todos)
     return
 
-
+'''
+probando un amend aquí.
+'''
 
 st.title("My web todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>",
+         unsafe_allow_html=True)
 
+
+st.text_input(label="",placeholder="Add new to_do",
+              on_change=add_todo,key='new_todo')
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo,key=todo)
     if checkbox:
@@ -32,8 +38,7 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="",placeholder="Add new to_do",
-              on_change=add_todo,key='new_todo')
+
 print ("Hello")
 print (session_state)
 st.session_state
